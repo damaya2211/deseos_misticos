2ff095a6b29356b96d394195064c7ac163ce9df8
Daniel <82892118+damaya2211@users.noreply.github.com>
2021-04-30 15:34:21 -0500

    Update App.py

--- App.py
+++ App.py
@@ -12,63 +12,6 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
    return render_template('index.html')
-    
-
-                    
-
-#nosotros
-
-@app.route('/nosotros')
-def Nosotros():
-   return render_template('nosotros.html')
-
-
-
-#Fin de nosotros
-
-
-#inicio index web
-@app.route('/inicio')
-def Inicio():
-   return render_template('inicio.html')
-
-
-#fin index web
-
-
-#principio servicios y añadir servicios
-
-@app.route('/servicios')
-def servicios():
-   return render_template('servicios.html') 
-
-
-
-         
-
-#Fin servicios y añadir servicios
-
-
-
-# principio contacto
-@app.route('/contacto')
-def Contacto():
-   return render_template('contacto.html')
-
-#fin contacto
-
-
-
-
-#principio blog y añadir blog
-
-@app.route('/blog')
-def blog():
-   return render_template('blog.html')
-
-         
-
-#Fin blog y añadir blog
 
 
 

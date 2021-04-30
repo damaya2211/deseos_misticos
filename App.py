939d021d59ce0f1aaa2a5aa4bc13fa42ffc95ddf
Daniel <82892118+damaya2211@users.noreply.github.com>
2021-04-30 15:15:45 -0500

    Update App.py

--- App.py
+++ App.py
@@ -3,79 +3,34 @@ from flask import flash
 from flask import redirect
 from flask import url_for
 from flask import render_template
-from flask import request
-from flask import jsonify
-from flask import send_from_directory
-
-import os 
 
 
 app = Flask(__name__)
 
 
 
-# Mysql Connection
-
-
-
-
-# settings
-app.secret_key = 'abjkhasd22Ht4lEms5289'
-
-
-
-   
-   
-   
-   
-#principio entrada admin
-
-
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-
-
-@app.errorhandler(404)
-def page_error(error):
-    return render_template("404.html"), 404
-
-
-
-
-# Inicio e Agregar contacto
-
-
 @app.route('/')
 def Index():
-        return render_template('index.html')
+   return render_template('index.html')
     
 
-                     
-  
-
-
+                    
 
 #nosotros
 
 @app.route('/nosotros')
 def Nosotros():
-    return render_template('nosotros.html')
+   return render_template('nosotros.html')
 
 
-
-@app.route('/ping')
-def ping():
-    return jsonify({"message": "pong"})
 
 #Fin de nosotros
+
 
 #inicio index web
 @app.route('/inicio')
 def Inicio():
-    return render_template('inicio.html')
+   return render_template('inicio.html')
 
 
 #fin index web
@@ -85,15 +40,10 @@ def Inicio():
 
 @app.route('/servicios')
 def servicios():
-    return render_template('servicios.html') 
+   return render_template('servicios.html') 
 
 
 
- 
- 
-@app.route('/admin')
-def createservicios():
-    return render_template('admin.html')
          
 
 #Fin servicios y añadir servicios
@@ -103,47 +53,9 @@ def createservicios():
 # principio contacto
 @app.route('/contacto')
 def Contacto():
-    return render_template('contacto.html')
+   return render_template('contacto.html')
 
 #fin contacto
-
-
-#principio calculadora
-
-@app.route('/calculadora', methods=["GET", "POST"])
-def calculadora():
-    if request.method=="GET":
-        return render_template("operar.html")
-    else:
-        try:
-            num1=int(request.form["num1"])
-            num2=int(request.form["num2"])
-        except:
-            abort(404)
-        op=request.form["operacion"]
-        if op=="sumar":
-            resultado=num1+num2
-            signo="+"
-        elif op=="sumar":
-            resultado=num1-num2
-            signo="-"
-        elif op=="multiplicar":
-            resultado=num1*num2
-            signo="*"
-        elif op=="dividir":
-            resultado=num1/num2
-            signo="/"
-        else:
-            abort(404)
-        return render_template('operar.html', numero1=num1, numero2=num2, operacion=op, signo=signo, resultado=resultado)
-                       
-            
-            
-
-
-#fin calculadora
-
-
 
 
 
@@ -152,10 +64,7 @@ def calculadora():
 
 @app.route('/blog')
 def blog():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM blog')
-    data = cur.fetchall()
-    return render_template('blog.html', articulo= data)
+   return render_template('blog.html', articulo= data)
 
          
 
@@ -171,4 +80,4 @@ def amarre():
 
 
 if __name__=='__main__':
-    app.run(port=7000, debug=True)
+    app.run(host="0.0.0.0" port=80, debug=True)

b197f5bf7bd3863e24042b9e606a9c141a38dcf7
Daniel <82892118+damaya2211@users.noreply.github.com>
2021-04-30 15:45:20 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from flask import flash
-from flask import redirect
-from flask import url_for
-from flask import render_template
 
 
 app = Flask(__name__)
@@ -11,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Index():
-   return render_template('index.html')
+   return'Hola mUndo'
 
 
 
